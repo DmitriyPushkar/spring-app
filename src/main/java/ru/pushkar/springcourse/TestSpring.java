514a720cd4d09c52515d8310c2e6c9ec22c04b79
Dmitriy Pushkarev <dum3712@mail.ru>
2023-05-03 13:23:42 +0400

    add dependency injection by constructor

--- src/main/java/ru/pushkar/springcourse/TestSpring.java
+++ src/main/java/ru/pushkar/springcourse/TestSpring.java
@@ -5,9 +5,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
-        Game game = context.getBean("gameBean", Game.class);
+//        Game game = context.getBean("gameBean", Game.class);
+//        Computer computer=new Computer(game);
 
-        Computer computer=new Computer(game);
+        Computer computer=context.getBean("computer", Computer.class);
         computer.playGame();
         context.close();
     }
