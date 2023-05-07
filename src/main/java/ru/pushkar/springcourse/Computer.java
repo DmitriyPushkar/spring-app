ad712043e53d03d38471409d141448e39beffa28
Dmitriy Pushkarev <dum3712@mail.ru>
2023-05-07 17:48:27 +0400

    add annotation @Qualifier

--- src/main/java/ru/pushkar/springcourse/Computer.java
+++ src/main/java/ru/pushkar/springcourse/Computer.java
@@ -1,21 +1,20 @@
 package ru.pushkar.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 @Component
 public class Computer {
-    private ArcadeGame arcadeGame;
-    private StrategyGame strategyGame;
-    private EctionGame ectionGame;
+ private Game game1;
+ public Game game2;
 @Autowired
-    public Computer(ArcadeGame arcadeGame, StrategyGame strategyGame, EctionGame ectionGame) {
-        this.arcadeGame = arcadeGame;
-        this.strategyGame = strategyGame;
-        this.ectionGame = ectionGame;
+    public Computer(@Qualifier("ectionGame") Game game1, @Qualifier("strategyGame") Game game2) {
+        this.game1 = game1;
+        this.game2 = game2;
     }
 
     public String playGame(){
-    return "Playing: " +arcadeGame.getPlay();
+    return "Playing: " +game1.getPlay()+" , "+ game2.getPlay();
 
 
     }
